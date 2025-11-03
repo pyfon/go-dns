1ec19a85c094edb05a09fdd75a7ef5aaaf2e5d67
Nathan Weetman-Ots <nathan@weetman.net>
2025-11-03 00:28:39 +0000

    WIP zone file processing

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+}
+
+func main() {
+	log.Info("Nathan's DNS Server")
+
+	if len(os.Args) <= 1 {
+		log.Error("Missing command line argument: path to a directory of zone files")
+		os.Exit(1)
+	}
+
+	zonePath := os.Args[1]
+	log.Debugf("Parsing zone files in %s", zonePath)
+
+	files, err := getFiles(zonePath)
+	if err != nil {
+		log.Errorf("Couldn't gather zone files in %v: %v", zonePath, err)
+		os.Exit(1)
+	}
+
+	var zones []Zone
+	for _, file := range files {
+		zoneFile, err := os.Open(file)
+		if err != nil {
+			log.Error(err.Error())
+			os.Exit(1)
+		}
+		zone, err := parseZone(zoneFile)
+		zoneFile.Close()
+		if err != nil {
+			log.Error(err.Error())
+			os.Exit(1)
+		}
+		zones = append(zones, zone)
+	}
+}
+
+// getFiles returns a list of valid, resolved file paths of all files recursively found under dirPath.
+func getFiles(dirPath string) ([]string, error) {
+	var files []string
+
+	evalDirEnt := func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		realPath, err := filepath.EvalSymlinks(path)
+		if err != nil {
+			return err
+		}
+		absPath, err := filepath.Abs(realPath)
+		if err != nil {
+			return err
+		}
+		files = append(files, absPath)
+		return nil
+	}
+
+	if err := filepath.WalkDir(dirPath, evalDirEnt); err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
---
+++ zone.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"io"
+)
+
+type Zone struct {
+}
+
+// parseZone returns a
+func parseZone(reader io.Reader) (Zone, error) {
+	return Zone{}, nil
+}
