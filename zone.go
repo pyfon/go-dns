b2e806b1b9ae7d6e609e83680f315e71a3594079
Nathan Weetman-Ots <nathan@weetman.net>
2026-03-06 20:04:29 +0000

    Move FindBestZoneMatch to zone.go, remove utils.go

--- zone.go
+++ zone.go
@@ -69,6 +69,25 @@ func NewTXTData(data string) TXTData {
 	return TXTData(out)
 }
 
+// FindBestZoneMatch finds the zone which is the most specific match for domain in the zone map
+// and returns a pointer to it.
+// For example a.b.example.com would first match the b.example.com zone if present, if not example.com, if not com.
+// This function will return nil if no match is found in the zones map.
+func FindBestZoneMatch(zones map[Domain]*Zone, domain Domain) *Zone {
+	curDomain := domain
+	for {
+		zone, ok := zones[curDomain]
+		if ok {
+			return zone
+		}
+		var tld bool
+		curDomain, tld = curDomain.Parent()
+		if tld {
+			return nil // We've hit the root with no matches.
+		}
+	}
+}
+
 // FQDN reports whether Domain is fully-qualified. It does not check for domain validity.
 func (d Domain) FQDN() bool {
 	return d[len(d)-1:] == "."
