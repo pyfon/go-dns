cc9448c399f3d7e7a28b225a39e591f23ed192e4
Nathan Weetman-Ots <nathan@weetman.net>
2026-03-10 00:33:20 +0000

    Add zone-gen

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 A simple DNS server written in Go.  
 This is an active work-in-progress.
 
-100% written by me, not an LLM :)
+100% written by me, not an LLM :)  
+
+`cmd/zone-gen` contains a simple Go program for generating large zone files for stress testing.
---
+++ cmd/zone-gen/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+const NUM_RECS = 1_000_000
+const THREADS = 8
+const STRLEN = 12
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+
+func randSeq() string {
+	n := STRLEN
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func GenRec(ch chan<- string) {
+	ch <- fmt.Sprintf("%s\tTXT\t\"%s\"\n", randSeq(), randSeq())
+}
+
+func main() {
+	fmt.Printf("zone %s.example.com\nttl 300\n\n", randSeq())
+
+	ch := make(chan string, NUM_RECS*THREADS)
+	var wg sync.WaitGroup
+
+	// Start goroutines
+	for i := 0; i < THREADS; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for x := 0; x < NUM_RECS/THREADS; x++ {
+				GenRec(ch)
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for s := range ch {
+		fmt.Printf("%s", s)
+	}
+}
